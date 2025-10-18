97c562c5afe5e8961d4bb595780fa834a2ad39cc
Muhammad Hashir <hashirmohamed04@gmail.com>
2025-10-18 21:14:44 +0530

    Initial Project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1 className="text-4xl">Welcome To FutureTech!</h1>
+    </div>
+  );
+}
