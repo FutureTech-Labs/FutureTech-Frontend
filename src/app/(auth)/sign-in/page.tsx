e582ca1b208d2ebdbac640617d1df3f1ad5c3022
Muhammad Hashir <hashirmohamed04@gmail.com>
2025-10-21 21:11:59 +0530

    setup custom colors

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignIn = () => {
+    return (
+        <h1 className='text-white text-xl'>Sign In</h1>
+    )
+}
+
+export default SignIn;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <div className="justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl">Welcome To FutureTech!</h1>
+    <div className="h-screen p-4">
+      <div className="grid grid-cols-5 grid-rows-2 gap-4 h-full">
+        {/* Left side (equal halves) */}
+        <div className="col-span-3 bg-amber-50">1</div>
+        <div className="col-span-3 bg-green-200 row-start-2">2</div>
+
+        {/* Right side (3 equal parts stacked) */}
+        <div className="col-span-2 row-span-2 col-start-4 flex flex-col gap-4">
+          <div className="flex-1 bg-blue-200">3</div>
+          <div className="flex-1 bg-red-200">4</div>
+          <div className="flex-1 bg-yellow-200">5</div>
+        </div>
+      </div>
     </div>
   );
 }
